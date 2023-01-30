02b68092175c3b1fbb74bb5b2a014d7472954a59
Christian Ermann <christianermann@gmail.com>
2023-01-30 18:46:15 -0500

    Added tufts-loop.js

---
+++ tufts-loop.js
@@ -0,0 +1,35 @@
+
+const WEB_LOOP_DATA_URL = `https://raw.githubusercontent.com/c2000e/web-loops/main/tufts-loop.json`;
+
+window.onload = function() {
+
+    const webLoop = document.getElementById("tufts-loop");
+    const thisSiteName = webLoop.getAttribute("site-name");
+
+    fetch(WEB_LOOP_DATA_URL)
+      .then((response) => response.json())
+      .then((sites) => {
+          const thisSiteIndex = sites.findIndex(
+              (site) => site.name === thisSiteName
+          );
+
+          let prevSiteIndex = thisSiteIndex - 1;
+          if (prevSiteIndex < 0) prevSiteIndex = sites.length - 1;
+
+          let nextSiteIndex = thisSiteIndex + 1;
+          if (nextSiteIndex > sites.length - 1) nextSiteIndex = 0;
+
+          const mainLink = webLoop.querySelector("#main");
+          const prevLink = webLoop.querySelector("#prev");
+          const nextLink = webLoop.querySelector("#next");
+
+          console.log(thisSiteIndex);
+          console.log(prevSiteIndex);
+          console.log(nextSiteIndex);
+
+          mainLink.href = sites[0].url;
+          prevLink.href = sites[prevSiteIndex].url;
+          nextLink.href = sites[nextSiteIndex].url;
+      });
+}
+
